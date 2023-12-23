5015a18a20da445986763b61306dcaac833e1698
Duzie <duzzi4x@gmail.com>
2023-12-24 00:36:53 +0100

    removed object id from price model

--- models/price-list.js
+++ models/price-list.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PriceList = new Schema({
-  _id: { type: String, required: true },
   product: String,
   category: String,
   unitPrice: Number,
